8d33e09ffb5782591ba3cbfe0b9bd8b033b6c596
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-11-13 05:13:30 +0100

    python plotting

--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream> 
 
 #include <nonlinfunc.hpp>
 #include <ode.hpp>
@@ -30,16 +31,23 @@ int main()
 {
   double tend = 4*M_PI;
   int steps = 100;
+  double tau = tend/steps;
+
   Vector<> y = { 1, 0 };  // initializer list
   auto rhs = std::make_shared<MassSpring>();
   
-  // ExplicitEuler stepper(rhs);
-  ImplicitEuler stepper(rhs);
+  ExplicitEuler stepper(rhs);
+  // ImplicitEuler stepper(rhs);
 
-  double tau = tend/steps;
+  std::ofstream outfile ("output_test_ode.txt");
+  std::cout << 0.0 << "  " << y(0) << " " << y(1) << std::endl;
+  outfile << 0.0 << "  " << y(0) << " " << y(1) << std::endl;
+
   for (int i = 0; i < steps; i++)
   {
      stepper.DoStep(tau, y);
+
      std::cout << (i+1) * tau << "  " << y(0) << " " << y(1) << std::endl;
+     outfile << (i+1) * tau << "  " << y(0) << " " << y(1) << std::endl;
   }
 }
