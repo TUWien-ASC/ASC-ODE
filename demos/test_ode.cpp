0757a4dfbd6265c8604c5281226a28241d9992c7
Joachim Schoeberl <joachim.schoeberl@tuwien.ac.at>
2025-11-13 09:11:30 +0100

    mass and stiffness

--- demos/test_ode.cpp
+++ demos/test_ode.cpp
@@ -9,20 +9,27 @@ using namespace ASC_ode;
 
 class MassSpring : public NonlinearFunction
 {
+private:
+  double mass;
+  double stiffness;
+
+public:
+  MassSpring(double m, double k) : mass(m), stiffness(k) {}
+
   size_t dimX() const override { return 2; }
   size_t dimF() const override { return 2; }
   
   void evaluate (VectorView<double> x, VectorView<double> f) const override
   {
     f(0) = x(1);
-    f(1) = -x(0);
+    f(1) = -stiffness/mass*x(0);
   }
   
   void evaluateDeriv (VectorView<double> x, MatrixView<double> df) const override
   {
     df = 0.0;
     df(0,1) = 1;
-    df(1,0) = -1;
+    df(1,0) = -stiffness/mass;
   }
 };
 
@@ -34,7 +41,7 @@ int main()
   double tau = tend/steps;
 
   Vector<> y = { 1, 0 };  // initializer list
-  auto rhs = std::make_shared<MassSpring>();
+  auto rhs = std::make_shared<MassSpring>(1.0, 1.0);
   
   ExplicitEuler stepper(rhs);
   // ImplicitEuler stepper(rhs);
